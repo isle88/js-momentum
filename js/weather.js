30e8aecb0f97a6eeacdce7ff06bc99e746220362
isle88 <hyunjungkim2014@gmail.com>
2022-06-06 10:50:38 +0100

    update api

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,9 @@
-const apiKey = config.API_KEY;
+import API_KEY from "./apikey.js";
 
 function onGeo(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
